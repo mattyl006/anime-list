58842735d6d01aa272e603660bf81e389109328b
Mateusz <wirus006@gmail.com>
2020-05-10 20:38:28 +0200

    Implement AnimeService and remove StartExample.java

--- src/main/java/com/wirus006/animelist/repositories/AnimeRepo.java
+++ src/main/java/com/wirus006/animelist/repositories/AnimeRepo.java
@@ -1,9 +1,17 @@
 package com.wirus006.animelist.repositories;
 
 import com.wirus006.animelist.entities.Anime;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AnimeRepo extends CrudRepository<Anime, Long> {
+public interface AnimeRepo extends CrudRepository<Anime, Long>, PagingAndSortingRepository<Anime, Long> {
+    @Query("select count(a) from Anime a where a.id = ?1")
+    Integer checkIfExist(Long id);
+
+    @Query("select count(a) from Anime a where a.status = ?1")
+    Integer showCountOfAnimeStatus(String type);
 }
---
+++ src/main/java/com/wirus006/animelist/services/AnimeService.java
@@ -0,0 +1,57 @@
+package com.wirus006.animelist.services;
+
+import com.wirus006.animelist.entities.Anime;
+import com.wirus006.animelist.repositories.AnimeRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class AnimeService {
+
+    private AnimeRepo animeRepo;
+
+    @Autowired
+    public AnimeService(AnimeRepo animeRepo) {
+        this.animeRepo = animeRepo;
+    }
+
+    public Iterable<Anime> listAllAnimePaging(Integer pageNr, Integer howManyOnPage) {
+        return animeRepo.findAll(PageRequest.of(pageNr, howManyOnPage));
+    }
+
+    public Optional<Anime> findById(Long id) {
+        return animeRepo.findById(id);
+    }
+
+    public Iterable<Anime> findAll() {
+        return animeRepo.findAll();
+    }
+
+    public Anime save(Anime anime) {
+        return animeRepo.save(anime);
+    }
+
+    public void deleteById(Long id) {
+        animeRepo.deleteById(id);
+    }
+
+    public Boolean checkIfExist(Long id) {
+        if (animeRepo.checkIfExist(id) > 0)
+            return true;
+        else return false;
+    }
+
+    public Integer showCountOfAnimeStatus(String status) {
+        return animeRepo.showCountOfAnimeStatus(status);
+    }
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void fillDB() {
+        save(new Anime("Prince of tennis", "Trans Arts", 2001, 178, "Completed"));
+    }
+}
