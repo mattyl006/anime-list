288387477041efc42ad61a941a4568e346017a5a
Mateusz <wirus006@gmail.com>
2020-05-10 16:35:37 +0200

    Initial commit

---
+++ src/main/java/com/wirus006/animelist/AnimeListApplication.java
@@ -0,0 +1,13 @@
+package com.wirus006.animelist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimeListApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnimeListApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wirus006/animelist/AnimeListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wirus006.animelist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimeListApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
