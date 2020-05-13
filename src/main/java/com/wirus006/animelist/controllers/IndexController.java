9ea0f699016dd676baf52e815cd8d60b0c869088
Mateusz <wirus006@gmail.com>
2020-05-13 19:05:37 +0200

    iterate Anime objects from list to frontend work

--- src/main/java/com/wirus006/animelist/controllers/IndexController.java
+++ src/main/java/com/wirus006/animelist/controllers/IndexController.java
@@ -1,18 +1,31 @@
 package com.wirus006.animelist.controllers;
 
 import com.wirus006.animelist.entities.Anime;
+import com.wirus006.animelist.services.AnimeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 @RequestMapping("/")
 public class IndexController {
+
+    private AnimeService animeService;
+
+    @Autowired
+    public IndexController(AnimeService animeService) {
+        this.animeService = animeService;
+    }
+
     @RequestMapping(value = "", method = RequestMethod.GET)
     String get(Model model) {
-        Anime anime = new Anime("Prince of tennis", "Trans Arts", 2001, 178, "Completed");
-        model.addAttribute("anime", anime);
+        List<Anime> animes = (List<Anime>) this.animeService.findAll();
+        model.addAttribute("animes", animes);
         return "anime";
     }
 }
