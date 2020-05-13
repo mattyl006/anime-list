5839375876d8eb3ddb9ac95436f949792a3a23c7
Mateusz <wirus006@gmail.com>
2020-05-14 00:32:59 +0200

    iterable by querySelectorAll and set color by content value not work

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,1 +1,11 @@
 console.log("hello world");
+
+var status = document.querySelectorAll('.content__item--status-js');
+
+for(var i = 0; i < status.length; i++){
+    console.log(status[i].innerHTML);
+    if( status[i].innerHTML === 'Watching') {
+        status[i].style.color = '#BF1A2F';
+        console.log('ten sam color');
+    }
+}
